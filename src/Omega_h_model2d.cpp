1f62e2635f6cedfe2be0cb62c243606149f74ce0
Cameron Smith <smithc11@rpi.edu>
2024-03-12 13:53:45 -0400

    static loader funcs

--- src/Omega_h_model2d.cpp
+++ src/Omega_h_model2d.cpp
@@ -2,7 +2,7 @@
 
 namespace Omega_h {
 
-Model2D::Model2D(Mesh2D& mesh) {
+Model2D Model2D::MeshModel2D_load(Mesh2D& mesh) {
 
 }
 
---
+++ src/Omega_h_simModel2d.cpp
@@ -0,0 +1,11 @@
+#include <SimModel.h>
+#include <SimUtil.h>
+#include "Omega_h_model2d.hpp"
+
+namespace Omega_h {
+
+Model2D Model2D::SimModel2D_load(std::string const& filename) {
+}
+  
+};
+}//end namespace Omega_h
