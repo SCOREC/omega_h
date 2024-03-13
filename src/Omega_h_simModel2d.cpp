8fdbc8381b681f28b14841e98dca1424c346df9a
Cameron Smith <smithc11@rpi.edu>
2024-03-13 14:57:59 -0400

    check if valid, start vtxIds

--- src/Omega_h_simModel2d.cpp
+++ src/Omega_h_simModel2d.cpp
@@ -8,13 +8,30 @@ bool isModel2D(pGModel mdl) {
   return (GM_numRegions(mdl) == 0);
 }
 
+bool isValid(pGModel mdl, bool checkGeo = false) {
+  const auto maxCheckLevel = 2; //may be expensive
+  auto geoCheck = checkGeo ? maxCheckLevel : 0;
+  pPList errList =  NULL;
+  const auto valid = 1;
+  return (GM_isValid(mdl,geoCheck,errList) == valid);
+}
+
+LOs getVtxIds(pGModel mdl) {
+  auto vtxIds = LOs(GM_numVertices(mdl));
+  return vtxIds;
+}
+
 Model2D Model2D::SimModel2D_load(std::string const& filename) {
   pNativeModel nm = NULL;
   pProgress p = NULL;
   pGModel g = GM_load(filename.c_str(), nm, p);
-  const char* msg = "Simmetrix GeomSim model is not 2D... exiting\n";
-  OMEGA_H_CHECK_MSG(isModel2D(g), msg);
-  return Model2D();
+  const char* msg2d = "Simmetrix GeomSim model is not 2D... exiting\n";
+  OMEGA_H_CHECK_MSG(isModel2D(g), msg2d);
+  const char* msgValid = "Simmetrix GeomSim model is not valid... exiting\n";
+  OMEGA_H_CHECK_MSG(isValid(g), msgValid);
+  auto mdl = Model2D();
+  mdl.vtxIds = getVtxIds(g);
+  return mdl;
 }
   
 }//end namespace Omega_h
