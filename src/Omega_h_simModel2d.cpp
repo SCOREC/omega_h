6bc481bdae65f802e786b20e06e9a300be096970
Cameron Smith <smithc11@rpi.edu>
2024-03-13 14:31:22 -0400

    check if 2d

--- src/Omega_h_simModel2d.cpp
+++ src/Omega_h_simModel2d.cpp
@@ -4,10 +4,16 @@
 
 namespace Omega_h {
 
+bool isModel2D(pGModel mdl) {
+  return (GM_numRegions(mdl) == 0);
+}
+
 Model2D Model2D::SimModel2D_load(std::string const& filename) {
   pNativeModel nm = NULL;
   pProgress p = NULL;
   pGModel g = GM_load(filename.c_str(), nm, p);
+  const char* msg = "Simmetrix GeomSim model is not 2D... exiting\n";
+  OMEGA_H_CHECK_MSG(isModel2D(g), msg);
   return Model2D();
 }
   
