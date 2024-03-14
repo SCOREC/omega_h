06580ce66148e1f1acb197dcf0a9b17faaf6dfc5
Cameron Smith <smithc11@rpi.edu>
2024-03-13 22:53:24 -0400

    reorganize data members, remove loops
    simmetrix does not store loops, only loop uses, remove them for now

--- src/Omega_h_model2d.hpp
+++ src/Omega_h_model2d.hpp
@@ -11,23 +11,28 @@ class Mesh2D;
 
 class Model2D {
 public:
+  //constructors
 #ifdef OMEGA_H_USE_SIMMODSUITE
   static Model2D SimModel2D_load(std::string const& filename);
 #endif
   static Model2D MeshModel2D_load(Mesh2D& mesh);
-
-  Real vtxTol, edgeTol;
-  Reals vtxCoords;
-  LOs vtxIds, edgeIds, loopIds, faceIds;
-  LOs looptoLoopUse;
+  //ids
+  LOs vtxIds /*x*/, edgeIds /*x*/, faceIds /*x*/;
+  //equal order adjacencies
   Graph edgeToEdgeUse;
+  //downward adjacencies
   Graph faceToLoopUse;
   Graph loopUseToEdgeUse;
   LOs edgeUseToVtx;
+  //upward adjacencies
   Graph vtxToEdgeUse;
   LOs edgeUseToLoopUse;
   LOs loopUseToFace;
+  //use orientation
   LOs edgeUseOrientation, loopUseOrientation;
+  //geometry
+  Real vtxTol, edgeTol;
+  Reals vtxCoords; //x
 private:
   Model2D() = default;
 };
