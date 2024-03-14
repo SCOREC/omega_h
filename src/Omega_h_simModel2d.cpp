9a891eff0ed183af3e2ce783230533f24ba37c0e
Cameron Smith <smithc11@rpi.edu>
2024-03-13 22:58:37 -0400

    face and edge ids, initial uses

--- src/Omega_h_simModel2d.cpp
+++ src/Omega_h_simModel2d.cpp
@@ -21,6 +21,59 @@ struct VtxIdsAndCoords {
   Reals coords;
 };
 
+LOs getFaceIds(pGModel mdl) {
+  auto numFaces = GM_numFaces(mdl);
+  auto ids_h = HostWrite<LO>(numFaces);
+  GFIter modelFaces = GM_faceIter(mdl);
+  int idx = 0;
+  pGFace modelFace;
+  while(modelFace=GFIter_next(modelFaces)) {
+    ids_h[idx++] = GEN_tag(modelFace);
+  }
+  GFIter_delete(modelFaces);
+  return LOs(ids_h);
+}
+
+LOs getUses(pGModel mdl) {
+  GFIter modelFaces = GM_faceIter(mdl);
+  int idx = 0;
+  pGFace modelFace;
+  while(modelFace=GFIter_next(modelFaces)) {
+    for(int side=0; side<2; side++) {
+      auto faceUse = GF_use(modelFace,side);
+      auto numLoops = GF_numLoops(modelFace);
+      auto loopUses = GFU_loopIter(faceUse);
+      pGLoopUse loopUse;
+      while(loopUse=GLUIter_next(loopUses)) {
+        auto edgeUses = GLU_edgeUseIter(loopUse);
+        pGEdgeUse edgeUse;
+        while(edgeUse=GEUIter_next(edgeUses)) {
+          auto edge = GEU_edge(edgeUse);
+          auto vtx0 = GE_vertex(edge,0);
+          auto vtx1 = GE_vertex(edge,1);
+        }
+        GEUIter_delete(edgeUses);
+      }
+      GLUIter_delete(loopUses);
+    } //sides
+  }
+  GFIter_delete(modelFaces);
+  return LOs();
+}
+
+LOs getEdgeIds(pGModel mdl) {
+  auto numEdges = GM_numEdges(mdl);
+  auto ids_h = HostWrite<LO>(numEdges);
+  GEIter modelEdges = GM_edgeIter(mdl);
+  int idx = 0;
+  pGEdge modelEdge;
+  while(modelEdge=GEIter_next(modelEdges)) {
+    ids_h[idx++] = GEN_tag(modelEdge);
+  }
+  GEIter_delete(modelEdges);
+  return LOs(ids_h);
+}
+
 VtxIdsAndCoords getVtxIdsAndCoords(pGModel mdl) {
   const auto numSpatialDims = 3;
   auto numVtx = GM_numVertices(mdl);
@@ -53,6 +106,9 @@ Model2D Model2D::SimModel2D_load(std::string const& filename) {
   auto vtxInfo = getVtxIdsAndCoords(g);
   mdl.vtxIds = vtxInfo.ids;
   mdl.vtxCoords = vtxInfo.coords;
+  mdl.edgeIds = getEdgeIds(g);
+  mdl.faceIds = getFaceIds(g);
+  getUses(g);
   return mdl;
 }
   
