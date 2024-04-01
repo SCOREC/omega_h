8eabe814e7fe1a802f494947690b16213a4d58af
Cameron Smith <smithc11@rpi.edu>
2024-04-01 14:11:40 -0400

    add remaining checks

--- src/simModel2dLoad.cpp
+++ src/simModel2dLoad.cpp
@@ -44,32 +44,33 @@ int main(int argc, char** argv) {
   Omega_h::LOs expectedFaceIds = {2};
   OMEGA_H_CHECK(model.faceIds == expectedFaceIds);
 
-  //use ids and their adjacencies are **not** accessible in SimModeler
+  //In SimModeler, use ids and their adjacencies are **not** accessible/visible
   Omega_h::LOs expectedEdgeUseIds = {8,6,4,2,1,3,5,7};
   OMEGA_H_CHECK(model.edgeUseIds == expectedEdgeUseIds);
   Omega_h::LOs expectedLoopUseIds = {2,1};
   OMEGA_H_CHECK(model.loopUseIds == expectedLoopUseIds);
 
+  //check adjacencies
   Omega_h::Graph expected_e2eu = Omega_h::Graph({0,2,4,6,8},{3,4,2,5,1,6,0,7});
   OMEGA_H_CHECK(model.edgeToEdgeUse == expected_e2eu);
 
+  Omega_h::LOs expected_eu2lu = {0,0,0,0,1,1,1,1};
+  OMEGA_H_CHECK(model.edgeUseToLoopUse == expected_eu2lu);
   Omega_h::Graph expected_lu2eu = Omega_h::Graph({0,4,8},{0,1,2,3,4,5,6,7});
   OMEGA_H_CHECK(model.loopUseToEdgeUse == expected_lu2eu);
 
-  //Omega_h::Graph expected_v2eu = Omega_h::Graph({0,4,8,12,16},
-  //                                              {0,1,6,7,
-  //                                               1,2,5,6,
-  //                                               2,3,4,5,
-  //                                               0,3,4,7});
-  //OMEGA_H_CHECK(model.vtxToEdgeUse == expected_v2eu); //FIXME
+  Omega_h::LOs expected_eu2v = {0,3,1,0,2,1,3,2,3,2,2,1,1,0,0,3};
+  OMEGA_H_CHECK(model.edgeUseToVtx == expected_eu2v);
+  Omega_h::Graph expected_v2eu = Omega_h::Graph({0,4,8,12,16},
+                                                {0,1,6,7,
+                                                 1,2,5,6,
+                                                 2,3,4,5,
+                                                 0,3,4,7});
+  OMEGA_H_CHECK(model.vtxToEdgeUse == expected_v2eu);
 
-  printLOs(model.edgeUseToVtx, 2, "edgeUseToVtx");
-  //FIXME add check for eu2v
-
+  Omega_h::LOs expected_lu2f = {0,0};
+  OMEGA_H_CHECK(model.loopUseToFace == expected_lu2f);
   Omega_h::Graph expected_f2lu = Omega_h::Graph({0,2},{0,1});
   OMEGA_H_CHECK(model.faceToLoopUse == expected_f2lu);
-
-  printGraph(model.faceToLoopUse, "faceToLoopUse");
-
   return 0;
 }
