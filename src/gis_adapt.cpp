e72023cb372166a9772adfdfc2460562ef464cab
Cameron Smith <smithc11@rpi.edu>
2024-05-29 10:51:56 -0400

    compute volume

--- src/gis_adapt.cpp
+++ src/gis_adapt.cpp
@@ -9,8 +9,73 @@
 #include "Omega_h_shape.hpp"
 #include "Omega_h_timer.hpp"
 #include <Omega_h_file.hpp> //Omega_h::binary
+#include <sstream> //ostringstream
+#include <iomanip> //precision
 
 using namespace Omega_h;
+
+template <typename T>
+void printTagInfo(Omega_h::Mesh& mesh, std::ostringstream& oss, int dim, int tag, std::string type) {
+    auto tagbase = mesh.get_tag(dim, tag);
+    auto array = Omega_h::as<T>(tagbase)->array();
+
+    Omega_h::Real min = get_min(array);
+    Omega_h::Real max = get_max(array);
+
+    oss << std::setw(18) << std::left << tagbase->name().c_str()
+        << std::setw(5) << std::left << dim 
+        << std::setw(7) << std::left << type 
+        << std::setw(5) << std::left << tagbase->ncomps() 
+        << std::setw(10) << std::left << min 
+        << std::setw(10) << std::left << max 
+        << "\n";
+}
+
+Reals getVolumeFromIceThickness(Omega_h::Mesh& mesh) {
+  auto coords = mesh.coords();
+  auto triArea = mesh.ask_sizes();
+  auto iceThickness = mesh.get_array<Real>(OMEGA_H_VERT, "ice_thickness");
+  auto faces2verts = mesh.ask_elem_verts();
+  Write<Real> vol(mesh.nfaces()); 
+  auto getVol = OMEGA_H_LAMBDA(LO faceIdx) {
+    auto triVerts = Omega_h::gather_verts<3>(faces2verts, faceIdx);
+    auto triThickness = Omega_h::gather_scalars<3>(iceThickness, triVerts);
+    Real avgThickness = 0;
+    for(LO i=0; i<3; i++)
+       avgThickness += triThickness[i];
+    avgThickness /= 3;
+    vol[faceIdx] = triArea[faceIdx]*avgThickness;
+  };
+  parallel_for(mesh.nfaces(), getVol);
+  return vol;
+}
+
+void printTags(Mesh& mesh) {
+    std::ostringstream oss;
+    // always print two places to the right of the decimal
+    // for floating point types (i.e., imbalance)
+    oss.precision(2);
+    oss << std::fixed;
+
+    if (!mesh.comm()->rank()) {
+        oss << "\nTag Properties by Dimension: (Name, Dim, Type, Number of Components, Min. Value, Max. Value)\n";
+        for (int dim=0; dim <= mesh.dim(); dim++)
+        for (int tag=0; tag < mesh.ntags(dim); tag++) {
+            auto tagbase = mesh.get_tag(dim, tag);
+            if (tagbase->type() == OMEGA_H_I8)
+                printTagInfo<Omega_h::I8>(mesh, oss, dim, tag, "I8");
+            if (tagbase->type() == OMEGA_H_I32)
+                printTagInfo<Omega_h::I32>(mesh, oss, dim, tag, "I32");
+            if (tagbase->type() == OMEGA_H_I64)
+                printTagInfo<Omega_h::I64>(mesh, oss, dim, tag, "I64");
+            if (tagbase->type() == OMEGA_H_F64)
+                printTagInfo<Omega_h::Real>(mesh, oss, dim, tag, "F64");
+        }
+
+        std::cout << oss.str();
+    }
+
+}
 
 static void check_total_mass(Mesh* mesh) {
   const Real iceDensity = 910; //kg/m^3
@@ -54,11 +119,17 @@ int main(int argc, char** argv) {
   opts.xfer_opts.integral_map["ice_thickness"] = "mass";
   opts.xfer_opts.integral_diffuse_map["mass"] =
       VarCompareOpts{VarCompareOpts::RELATIVE, 0.9, 0.0};
+  opts.xfer_opts.validate(&mesh);
   check_total_mass(&mesh);
 
+  auto vol = getVolumeFromIceThickness(mesh);
+  mesh.add_tag(2, "volume", 1, vol);
   printTriCount(&mesh);  
+  printTags(mesh);
+  Omega_h::vtk::write_parallel("beforeAdapt.vtk", &mesh, 2);
   adapt(&mesh, opts);
   printTriCount(&mesh);  
+  printTags(mesh);
   check_total_mass(&mesh);
   const std::string vtkFileName = std::string(argv[2]) + ".vtk";
   Omega_h::vtk::write_parallel(vtkFileName, &mesh, 2);
