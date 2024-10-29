f91dca955e24c6b83c8dbb1896abc45ea91130cd
Cameron Smith <smithc11@rpi.edu>
2024-10-29 11:00:15 -0400

    wip: different attempts

--- src/reduce_test.cpp
+++ src/reduce_test.cpp
@@ -8,28 +8,46 @@ int main(int argc, char** argv) {
   {
     struct sum_reduce {
       using value_type=LO;
-      void init(value_type& res) {
-        res = 0;
-      }
+      OMEGA_H_INLINE void init(value_type& update) const { update = 0; }
       OMEGA_H_INLINE
-      void operator()(const LO& i, LO& lsum) const {
+      void operator()(const value_type& i, LO& lsum) const {
         lsum += i;
       }
+      OMEGA_H_INLINE void join(value_type& update, const value_type& input) const {
+        update = update + input;
+      }
     };
-  {
+
+    LO n = 5;
+    LO expected = 10;
+    {
+    //this doesn't compile - cuda 'invalid template' compilation error, the
+    //source of the error isn't obvious
     sum_reduce sr;
-    sum_reduce::value_type result;
-    sr.init(result);
-    LO lsum=0;
-    LO two=2;
-    sr(two, lsum);
-  }
-    sum_reduce sr;
-    LO n = 3;
-    auto result = parallel_reduce<sum_reduce>(n, sr, "my_reduce");
-    //auto kernel = OMEGA_H_LAMBDA(const LO& i, LO& lsum) { lsum += i; };
-    //auto result = parallel_reduce(n, kernel, "reduce");
+    auto result = Omega_h::parallel_reduce<sum_reduce>(n, sr, "my_reduce");
     fprintf(stderr, "result %d\n", result);
+    assert(result == expected);
+    }
+
+    {
+    //this doesn't compile - invalid args to Omega_h::parallel_reduce
+    //I wouldn't expect it to compile as it doesn't define 'init()' or 'value_type'
+    //neded by Omega_h::parallel_reduce.
+    //auto kernel = OMEGA_H_LAMBDA(const LO& i, LO& lsum) { lsum += i; };
+    //auto result = Omega_h::parallel_reduce(n, kernel, "my_reduce");
+    //fprintf(stderr, "result %d\n", result);
+    //assert(result == expected);
+    }
+
+    {
+    //this works
+    int result = 0;
+    auto kernel = OMEGA_H_LAMBDA(const LO& i, LO& lsum) { lsum += i; };
+    Kokkos::parallel_reduce("my_reduce", policy(n), kernel, result);
+    fprintf(stderr, "result %d\n", result);
+    assert(result == expected);
+    }
+
   }
   return 0;
 }
