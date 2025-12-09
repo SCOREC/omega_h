9e00c137b93cf05b0e7fcfc185a450c24503758a
Cameron Smith <smithc11@rpi.edu>
2025-12-09 14:57:53 -0500

    wip: compiles [skip ci]

--- src/Omega_h_bsplineModel2d.cpp
+++ src/Omega_h_bsplineModel2d.cpp
@@ -46,40 +46,58 @@ namespace {
 namespace Omega_h {
 
   Reals bspline_get_snap_warp(Mesh* mesh, BsplineModel2D* mdl, bool verbose) {
-    std::cerr << __func__ << "\n";
     OMEGA_H_CHECK(mesh->dim() == 2);
     auto class_dims = mesh->get_array<I8>(VERT, "class_dim");
     auto class_ids = mesh->get_array<ClassId>(VERT, "class_id");
-    auto class_parametric = mesh->get_array<ClassId>(VERT, "class_parametric");
-    auto coords = mesh->coords();
-    auto warp = Write<Real>(mesh->nverts() * 2);
+    auto class_parametric = mesh->get_array<Real>(VERT, "class_parametric");
+    OMEGA_H_CHECK(class_parametric.size() == mesh->nverts()*2);
 
-    //will new vertices have the 'class_parametric' tag set? if so what will it
-    //be? 0?
+    const auto sideDim = mesh->dim()-1;
+    auto sidesToMeshVerts = mesh->ask_revClass_downAdj(sideDim, VERT);
 
-    //fill the array of samplePts - need to loop through mesh verts, count those
-    //on the boundary, then run eval on those - will need to store an array of
-    //vertex indices so the following loop to set the warp vector only iterates
-    //over the boundary... or use reverse classification
-    const auto edgeToSamples = Omega_h::offset_scan(Omega_h::read(samplesPerEdge));
-    const int numSamples = edgeToSamples.last();
-    Omega_h::Write<Omega_h::Real> samplePts(numSamples, "splineSamplePoints");
-    auto ids = Write<Omega_h::LO> ids(numEdges, 0, 1, "splineIds");
-    const auto pts = model.eval(ids,edgeToSamples,samplePts);
+    //get the location of mesh vertices along the geometric model entity they
+    //are classified on using their parametric coordinates along that model
+    //entity
+    const auto sidesToSamples = sidesToMeshVerts.a2ab;
+    const auto meshEntIds = sidesToMeshVerts.ab2b;
+    const int numSides = sidesToSamples.size()-1;
+    const int numSamples = sidesToSamples.last();
+    auto sideIds = Write<Omega_h::LO>(numSides, 0, 1, "splineIds");
+    auto setSideIds = OMEGA_H_LAMBDA(Omega_h::LO side) {
+      //all mesh entities associated with this side will have the same side id,
+      //use the first one to store the id
+      const auto firstMeshEnt = meshEntIds[sidesToSamples[side]];
+      sideIds[side] = class_ids[firstMeshEnt];
+    };
+    Omega_h::parallel_for(sidesToSamples.size(), setSideIds, "setSideIds");
 
-    //create array of parametric points to query on the splines
-    Omega_h::parallel_for(mesh->nverts(), OMEGA_H_LAMBDA(Omega_h::LO i) {
-      auto currentPt = get_vector<2>(coords, i);
-      Int class_dim = class_dims[i];
-      OMEGA_H_CHECK(class_dim >= 0);
-      OMEGA_H_CHECK(class_dim <= 2);
-      auto d = vector_3(0, 0, 0);
-      if (class_dim == mesh->dim()-1) { //classified on model edges
-        const auto targetPt = pts[i]; //FIXME - what is the index for pts?
-        d = targetPt - currentPt;
+    auto samplePts = Omega_h::Write<Omega_h::Real>(numSamples, "splineSamplePoints");
+    auto setSamplePts = OMEGA_H_LAMBDA(Omega_h::LO side) {
+      for( auto ab = sidesToSamples[side]; ab < sidesToSamples[side+1]; ab++ ) {
+         const auto meshEnt = meshEntIds[ab];
+         //class_parametric is a tag with two components per ent
+         //only need first component for parametric coord along a spline
+         samplePts[ab] = class_parametric[meshEnt*2];
       }
-      set_vector(warp, i, d);
-    }
+    };
+    Omega_h::parallel_for(sidesToSamples.size(), setSamplePts, "setSamplePts");
+
+    const auto pts = mdl->eval(sideIds,sidesToSamples,samplePts);
+
+    //compute warp vector for mesh vertices classified on the model boundary (sides)
+    auto coords = mesh->coords();
+    auto warp = Write<Real>(mesh->nverts() * 2, 0);
+    auto setWarpVectors = OMEGA_H_LAMBDA(Omega_h::LO side) {
+      for( auto ab = sidesToSamples[side]; ab < sidesToSamples[side+1]; ab++ ) {
+         const auto meshEnt = meshEntIds[ab];
+         const auto currentPt = get_vector<2>(coords, meshEnt);
+         const auto targetPt = get_vector<2>(pts, meshEnt);
+         auto d = vector_2(0, 0);
+         d = targetPt - currentPt;
+         set_vector(warp, meshEnt, d);
+      }
+    };
+    Omega_h::parallel_for(sidesToSamples.size(), setWarpVectors, "setWarpVectors");
     return warp;
   }
 
